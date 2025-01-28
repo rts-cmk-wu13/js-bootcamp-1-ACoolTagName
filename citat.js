e566e508f7090d67027fa17f3c52b65a0e63b334
Geik <grigoriosvousios@gmail.com>
2025-01-28 11:29:43 +0100

    first commit added js

---
+++ citat.js
@@ -0,0 +1,25 @@
+let quots = [
+    "“Be yourself; everyone else is already taken.” ― Oscar Wilde",
+    "“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.” ― Marilyn Monroe",
+    "“So many books, so little time.” ― Frank Zappa",
+    "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.” ― Albert Einstein",
+    "“A room without books is like a body without a soul.” ― Marcus Tullius Cicero",
+    "“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.” ― Bernard M. Baruch",
+    "“You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth.” ― William W. Purkey",
+    "“You know you're in love when you can't fall asleep because reality is finally better than your dreams.” ― Dr. Seuss",
+    "“You only live once, but if you do it right, once is enough.” ― Mae West",
+    "“Be the change that you wish to see in the world.” ― Mahatma Gandhi",
+];
+let quetsListElm = document.querySelector(".quotelist");
+
+
+quots.forEach(function (quot) {
+
+    catsListElm.innerHTML += `<li class="catlist_item">$<{quot}</li>`;
+
+})
+
+
+
+
+Math.floor(Math.random() * 10);
